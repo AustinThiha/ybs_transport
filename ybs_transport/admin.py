b6e7270e0e2ca76a6ab64e0054a90cf88bf3159f
Kyaw Thiha <kyawkyawthiha.ppt@gmail.com>
2023-03-08 20:25:21 +0630

    xd

--- ybs_transport/admin.py
+++ ybs_transport/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bus, Route, BusWithRoute, Stop, RouteWithStop
 
 # Register your models here.
+admin.site.register(Bus)
+admin.site.register(Route)
+admin.site.register(BusWithRoute)
+admin.site.register(Stop)
+admin.site.register(RouteWithStop)
